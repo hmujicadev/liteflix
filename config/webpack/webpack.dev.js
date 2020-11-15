75dd9356ff2b8b2b708eb3ae879d359635854996
hmujica <hmujicadev@gmail.com>
2020-11-15 00:32:09 -0300

    add config webpack, react, babel, pretier, eslint

---
+++ config/webpack/webpack.dev.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const {merge} = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, '../../build'),
+    port: 8888,
+    historyApiFallback: true,
+    watchOptions: {
+      aggregateTimeout: 200,
+      poll: 1000,
+    },
+  },
+  devtool: 'source-map',
+});
